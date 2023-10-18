9f3ebb17116ce5a19b095e946fdefd9b3c045053
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-10-18 14:34:26 +0200

    praesedium

---
+++ app/_component/_themeAble/contactCard/contactCard.ts
@@ -0,0 +1,32 @@
+import declareComponent from "../../../lib/declareComponent"
+import ThemeAble from "../themeAble"
+import { BodyTypes } from "./pugBody.gen"; import "./pugBody.gen"
+
+export default class ContactCard extends ThemeAble {
+  protected body: BodyTypes
+
+  constructor() {
+    super(false)
+  }
+
+  personName(to: string) {
+    this.body.heading.text(to)
+  }
+
+  position(to: string) {
+    this.body.subTxt.text(to)
+  }
+
+  pic(src: string) {
+    this.body.pic.src(src)
+  }
+
+  stl() {
+    return super.stl() + require("./contactCard.css").toString()
+  }
+  pug() {
+    return require("./contactCard.pug").default
+  }
+}
+
+declareComponent("c-contact-card", ContactCard)
