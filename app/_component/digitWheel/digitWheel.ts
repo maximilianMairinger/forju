cc178236dac67b51513f293c53ab4445fce0e091
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-07-31 11:32:01 +0200

    digitWheel housekeeping and selectAble handeling

--- app/_component/digitWheel/digitWheel.ts
+++ app/_component/digitWheel/digitWheel.ts
@@ -5,25 +5,53 @@ import { Borrow } from "more-maps";
 import keyIndex from "key-index";
 import { clamp, probRange } from "../../lib/util";
 
-type Elem = {elem: HTMLElement, curVal: number, setVal(n: number): void}
+
+const translateEndTemplate = "px)"
+class Elem {
+  curVal: number
+  private isCurSelectAble = true
+  private translateTemplate: `translate${"X" | "Y"}(`
+  constructor(public elem: HTMLElement, dir: "y" | "x" = "y") {
+    this.translateTemplate = `translate${dir.toUpperCase()}(` as any
+  }
+
+  setPos(n: number) {
+    this.elem.style.transform = this.translateTemplate + n + translateEndTemplate
+    return this
+  }
+  setVal(n: number) {
+    n = +n
+    if (this.curVal === n) return this
+    this.elem.txt(n.toString(), false)
+    this.curVal = n
+    return this
+  }
+  setSelectable(selectAble: boolean) {
+    if (this.isCurSelectAble === selectAble) return this
+    this.isCurSelectAble = selectAble
+    this.elem.css("userSelect", selectAble ? "auto" : "none")
+    return this
+  }
+}
+
+class ElemIndex extends Array<Elem> {
+  getElemFromVal(val: number) {
+    if (this[0]?.curVal === val) return this[0]
+    if (this[1]?.curVal === val) return this[1]
+  }
+  setElemValLazy(val1?: number, val2?: number) {
+    if (val1 !== undefined) this[0].setVal(val1)
+    if (val2 !== undefined) this[1].setVal(val2)
+    return this
+  }
+}
 
 export default class DigitWheel extends Component<false> {
   protected body: BodyTypes
-  private elemIndex = [{
-    elem: this.mkElem(),
-    setVal(val: number) {
-      (this.elem as HTMLElement).txt(val.toString(), false)
-      this.curVal = val
-    },
-    curVal: undefined
-  }, {
-    elem: this.mkElem(),
-    setVal(val: number) {
-      (this.elem as HTMLElement).txt(val.toString(), false)
-      this.curVal = val
-    },
-    curVal: undefined
-  }] as any as [Elem, Elem] & {getElemFromVal(val: number): Elem, setElemValLazy(val1?: number, val2?: number): [Elem, Elem]}
+  private elemIndex = new ElemIndex(
+    new Elem(this.mkElem()),
+    new Elem(this.mkElem())
+   )
 
 
 
@@ -39,17 +67,6 @@ export default class DigitWheel extends Component<false> {
 
   constructor(initValue?: number) {
     super(false)
-
-    const elemIndex = this.elemIndex
-    elemIndex.getElemFromVal = function(val: number) {
-      if (elemIndex[0]?.curVal === val) return elemIndex[0]
-      if (elemIndex[1]?.curVal === val) return elemIndex[1]
-    }
-    elemIndex.setElemValLazy = function(val1?: number, val2?: number) {
-      if (val1 !== undefined && elemIndex[0]?.curVal !== val1) elemIndex[0].setVal(val1)
-      if (val2 !== undefined && elemIndex[1]?.curVal !== val2) elemIndex[1].setVal(val2)
-      return this as any
-    }
 
     if (initValue !== undefined) this.value(initValue)
 
@@ -128,22 +145,17 @@ export default class DigitWheel extends Component<false> {
 
         const distanceToPrev = ((toExact / digitExp) % topCutExp) - prevValCut
 
-        if (digit === 0) {
-          const myDistanceToPrev = distanceToPrev
-          this.elemIndex[0].elem.style.transform = `translateY(${myDistanceToPrev * this.elemHeight}px)`
-          this.elemIndex[1].elem.style.transform = `translateY(${-1 * (1 - myDistanceToPrev) * this.elemHeight}px)`
+
+        const thresholdForAnimation = 1 - 10**-digit
+        if (distanceToPrev/* % 10**digit */ >= thresholdForAnimation) {
+          const myDistanceToPrev = ((distanceToPrev - thresholdForAnimation) * digitExp) % 1 // should have a range of 0-1 now again
+          const is0PrimarilyOnScreen = myDistanceToPrev < .5
+          this.elemIndex[0].setSelectable(is0PrimarilyOnScreen).setPos(myDistanceToPrev * this.elemHeight)
+          this.elemIndex[1].setSelectable(!is0PrimarilyOnScreen).setPos(-1 * (1 - myDistanceToPrev) * this.elemHeight)
         }
         else {
-          const thresholdForAnimation = 1 - 10**-digit
-          if (distanceToPrev/* % 10**digit */ >= thresholdForAnimation) {
-            const myDistanceToPrev = ((distanceToPrev - thresholdForAnimation) * digitExp) % 1 // should have a range of 0-1 now again
-            this.elemIndex[0].elem.style.transform = `translateY(${myDistanceToPrev * this.elemHeight}px)`
-            this.elemIndex[1].elem.style.transform = `translateY(${-1 * (1 - myDistanceToPrev) * this.elemHeight}px)`
-          }
-          else {
-            this.elemIndex[0].elem.style.transform = `translateY(0px)`
-            this.elemIndex[1].elem.style.transform = `translateY(${this.elemHeight}px)`
-          }
+          this.elemIndex[0].setSelectable(true).setPos(0)
+          this.elemIndex[1].setSelectable(false).setPos(this.elemHeight)
         }
         
         
