d10a48383d4014137f85d13b19a8458e7d740ce7
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-11-18 10:18:30 +0100

    update prod server with tracking. Updated server setup to make this possible

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,35 @@
 import setup from "./setup"
+import sani from "sanitize-against"
+
+const saniQr = sani({
+  qrName: String
+})
 
 
+const qrCollectionName = process.env.inProd === "true" ? "qr" : "qrDev"
 
-setup().then(async (app) => {
-
+setup("forju").then(async ({app, db}) => {
+  (function rqHandling() {
+    const redirectMap = new Map()
+    redirectMap.set("workshop1", "/projects/schulworkshops/")
+    redirectMap.set("workshop2", "/projects/schulworkshops/")
+    redirectMap.set("workshop", "/projects/schulworkshops/")
+    redirectMap.set("fwdWXBC", "https://www.bildungschancen.wien/angebote/?tx_solr%5Bq%5D=For+Ju") // wien extra bildungschance
+    app.get("/qr/:qrName", (req, res, next) => {
+      try {
+        const { qrName } = saniQr(req.params)
+        console.log(qrName)
+        if (db !== undefined) db.collection(qrCollectionName).updateOne({ qrName }, { $inc: { count: 1 } }, { upsert: true })
+        else console.log(`QRVIEW: ${qrName}`)
+        const redirect = redirectMap.get(qrName)
+        if (redirect !== undefined) res.redirect(redirect)
+        else res.redirect("/")
+      }
+      catch(e) {
+        res.status(400)
+      }
+    })
+  })()
 
 })
+
