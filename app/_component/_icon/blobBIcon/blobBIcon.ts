f4dc85b39675c0b03698a75c6925465522510bcb
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2026-01-28 03:22:53 +0100

    work on the blob bg

--- app/_component/_icon/blobBIcon/blobBIcon.ts
+++ app/_component/_icon/blobBIcon/blobBIcon.ts
@@ -2,6 +2,8 @@ import Icon from "../icon";
 import declareComponent from "../../../lib/declareComponent";
 
 export default class BlobBIcon extends Icon {
+  public draggingSpeedFactor = .4
+  public totalMovementFactor = .6
   pug() {
     return require("./blobBIcon.pug").default
   }
--- app/_component/blobAndGlassBackground/blobAndGlassBackground.ts
+++ app/_component/blobAndGlassBackground/blobAndGlassBackground.ts
@@ -1,4 +1,6 @@
 import declareComponent from "../../lib/declareComponent"
+import BlobAIcon from "../_icon/blobAIcon/blobAIcon";
+import BlobBIcon from "../_icon/blobBIcon/blobBIcon";
 import Component from "../component"
 import { BodyTypes } from "./pugBody.gen"; import "./pugBody.gen"
 
@@ -8,8 +10,11 @@ export default class BlobAndGlassBackground extends Component {
   constructor() {
     super()
 
+    const blobA = new BlobAIcon().addClass("blob").addClass("a")
+    const blobB = new BlobBIcon().addClass("blob").addClass("b")
 
-
+    this.append(blobA)
+    this.append(blobB)
   }
 
   async loadAnimations() {
