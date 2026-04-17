f0caac44dbb7ac58a0aff3aab8d84fd7a14e60db
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2026-04-17 13:52:07 +0200

    video on landing page lol

--- app/_component/_themeAble/_frame/_pageSection/landingSection/landingSection.ts
+++ app/_component/_themeAble/_frame/_pageSection/landingSection/landingSection.ts
@@ -14,6 +14,7 @@ import "./../../../../_icon/spikeIcon/spikeIcon"
 import "./../../../../_icon/crossIcon/crossIcon"
 import "./../../../../_icon/ovalIcon/ovalIcon"
 import "./../../../textBlob/textBlob"
+import { getCurrentLoadRecord } from "../../frame"
 
 import * as isSafari from "is-safari"
 
@@ -27,9 +28,13 @@ export default class LandingSection extends PageSection {
   private coverButton = this.q("view-work .coverButton") as Button
   private rippleButton = this.q("view-work .rippleButton") as RippleButton
   private link = this.q("view-work c-link") as Link
+  private heroParallax = this.q("right-side c-parallax") as HTMLElement
+  private heroVideo = this.q("img-wrapper video") as unknown as HTMLVideoElement
   constructor() {
     super("light")
     if (isSafari) this.addClass("safari")
+
+    this.setupHeroVideoMorph()
 
     new EventListener(this.coverButton, ["mouseover", "focusin"], this.link.mouseOverAnimation)
     new EventListener(this.coverButton, ["mouseleave", "focusout"], this.link.mouseOutAnimation)
@@ -55,6 +60,70 @@ export default class LandingSection extends PageSection {
       })
 
     })
+  }
+
+  private setupHeroVideoMorph() {
+    if (!this.heroParallax || !this.heroVideo) return
+
+    const fullRecord = getCurrentLoadRecord()
+    const video = this.heroVideo
+    let playbackStarted = false
+    let revealStarted = false
+
+    video.loop = false
+    video.preload = "none"
+    video.muted = true
+    video.playsInline = true
+
+    fullRecord.full.add(async () => {
+      const deferredSrc = video.getAttribute("data-src")
+      if (!deferredSrc || video.getAttribute("src")) return
+      video.setAttribute("src", deferredSrc)
+      video.preload = "auto"
+      video.load()
+    })
+
+    const revealVideo = () => {
+      if (revealStarted) return
+      revealStarted = true
+      this.addClass("videoReveal")
+    }
+
+    const startPlayback = async () => {
+      if (!video.getAttribute("src")) return
+      if (playbackStarted) return
+      playbackStarted = true
+
+      if (video.readyState < HTMLMediaElement.HAVE_CURRENT_DATA) {
+        await new Promise<void>((res) => {
+          const done = () => res()
+          video.addEventListener("canplay", done, { once: true })
+          video.addEventListener("loadeddata", done, { once: true })
+        })
+      }
+
+      await video.play()
+    }
+
+    const revealAndQueuePlayback = () => {
+      this.addClass("imageMorph")
+      // Let the still image tilt/zoom first, then fade in the already-playing video.
+      delay(200, () => {
+        startPlayback()
+          .then(revealVideo)
+          .catch(() => {
+            // Hover-triggered playback can still be blocked on some browsers.
+            revealVideo()
+          })
+      })
+    }
+
+    this.heroParallax.addEventListener("mouseenter", revealAndQueuePlayback, { once: true })
+
+    video.addEventListener("ended", () => {
+      this.addClass("videoFinished")
+      video.pause()
+    }, { once: true })
   }
 
   stl() {
